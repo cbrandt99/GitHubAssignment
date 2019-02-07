18f4aa3779411f0388b0885e1265d9a05d4dc130
Christopher Brandt <christopherbrandt99.cb@gmail.com>
2019-02-07 11:13:01 -0500

    this is my first commit

---
+++ src/githubassignment/GitHubAssignment.java
@@ -0,0 +1,15 @@
+
+package githubassignment;
+
+
+public class GitHubAssignment {
+
+    
+    public static void main(String[] args) {
+       Brandt();
+    }
+    public static void Brandt(){
+        System.out.println("Christopher Brandt!!");
+    }
+    
+}
